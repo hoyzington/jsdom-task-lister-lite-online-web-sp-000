65abb1f1c4ccc3b93fcc527d39b02df9e9a07fc2
Scott Hoisington <hoyzington@gmail.com>
2020-11-28 07:10:40 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
-<<<<<<< HEAD
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById('create-task-form')
-  form.addEventListener('submit', (e) => addTask(e))
-=======
-document.addEventListener('submit', (e) => {
-  e.preventDefault;
-  let task = document.getElementById('')
->>>>>>> 06a9fb0fc133729fd7d5063e80400ef6445b7747
+  const form = document.getElementById('create-task-form');
+  form.addEventListener('submit', (e) => addTask(e));
 });
 
 function addTask(e) {
-  e.preventDefault()
-  const task = document.getElementById('new-task-description')
-  const ul = document.getElementById('tasks')
-  const li = document.createElement('li')
-  li.innerText = task.value
-  ul.appendChild(li)
-  e.target.reset()
+  e.preventDefault();
+  const task = document.getElementById('new-task-description');
+  const ul = document.getElementById('tasks');
+  const li = document.createElement('li');
+  li.innerText = task.value;
+  ul.appendChild(li);
+  e.target.reset();
 }
